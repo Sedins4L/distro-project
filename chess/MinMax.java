eaf289cd0801f2b808b1c818b53d463329a98c89
Spencer C. Gabhart <sgabhart2230@gmail.com>
2018-04-30 16:04:56 -0400

    Organization and dead code removal

--- chess/MinMax.java
+++ chess/MinMax.java
@@ -20,15 +20,24 @@ public class MinMax {
         rand = new Random();
     }
 
-	// Alpha-beta pruning methods
+	/*
+	 * Alpha-beta pruning methods. These functions examine all possible moves for Player 1 (alpha) and responses by Player 2 (beta).
+	 * The objective is to minimize the beta score while maximizing alpha. When we come to a beta that can't possibly exceed alpha,
+	 * we need not explore the remaining moves any further.
+	 */
     private float maxValue(Board b, ArrayList<Move> state, float alpha, float beta, int depth) {
-        if(depth > maxDepth)
-            return eval1(b, state, color);
+        // Per depth constraints, we cannot explore any deeper.
+		if(depth > maxDepth)
+            return powerIndex(b, state, color);
 
+		// Obtain all possible moves for this color based on board configuration.
         ArrayList<Move> moves = b.getMovesAfter(color, state);
+		// No possible move.
         if(moves.size() == 0)
             return Float.NEGATIVE_INFINITY;
 
+		// Explore the board one set of moves at a time.
+		// We pass back and forth between min/max to determine alpha and beta values.
         for(int i = 0; i < moves.size(); i++) {
             state.add(moves.get(i));
             float tmp = minValue(b, state, alpha, beta, depth + 1);
@@ -37,6 +46,8 @@ public class MinMax {
                 alpha = tmp;
             }
 
+			// Pruning.
+			// When beta can no longer exceed alpha, there is no need to continue examining this portion of the moves List.
             if(beta <= alpha)
                 break;
         }
@@ -44,9 +55,10 @@ public class MinMax {
         return alpha;
     }
 
+	// Veritably symmetrical functionality to maxValue.
     private float minValue(Board b, ArrayList<Move> state, float alpha, float beta, int depth) {
         if(depth > maxDepth)
-            return eval1(b, state, !color);
+            return powerIndex(b, state, !color);
 
         ArrayList<Move> moves = b.getMovesAfter(!color, state);
         if(moves.size() == 0) 
@@ -59,7 +71,7 @@ public class MinMax {
             if(tmp < beta) {
                 beta = tmp;
             }
-
+			
             if(beta <= alpha)
                 break;
         }
@@ -76,13 +88,18 @@ public class MinMax {
         if(moves.size() == 0)
             return null;
 
+		// Container for values obtained from min/max with pruning.
+		// This will be concurrently accessed, we must qualify the data type as such (Future).
         Vector<Future<Float>> costs = new Vector<Future<Float>>(moves.size());
         costs.setSize(moves.size());
 
+		// Create a number of threads up to the amount contained in the list of possible moves.
+		// Depending on hardware limitations, this drastically improves decision making time.
         ExecutorService exec = Executors.newFixedThreadPool(moves.size());
         try {
             for (int i = 0; i < moves.size(); i++) {
                 final Move move = moves.get(i);
+				// The values to be calculated in parallel.
                 Future<Float> result = exec.submit(new Callable<Float>() {
 
                     @Override
@@ -90,6 +107,8 @@ public class MinMax {
                         ArrayList<Move> state = new ArrayList<Move>();
                         state.add(move);
 
+						// Begin numerical analysis of move options with a call to min.
+						// Viable moves will be whittled down based on min/max evaluation.
                         float tmp = minValue(b, state, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY, 1);
                         return tmp;
                     }
@@ -100,8 +119,10 @@ public class MinMax {
             exec.shutdown();
         }
 
-        // max
-        int maxi = -1;
+        // Search costs Vector for best option.
+		// Positon of best
+        int maxPos = -1;
+		// Actual best
         float max = Float.NEGATIVE_INFINITY;
         for(int i = 0; i < moves.size(); i++) {
             float cost;
@@ -114,21 +135,23 @@ public class MinMax {
                 }
                 continue;
             }
+			// Randomization to help prevent stalemates, identical moves being chosen every game.
             if(cost >= max) {
-                if(Math.abs(cost-max) < 0.1) // add a little random element
+                if(Math.abs(cost-max) < 0.1) 
                     if(rand.nextBoolean())
                         continue;
 
                 max = cost;
-                maxi = i;
+                maxPos = i;
             }
         }
 
-        return moves.get(maxi);
+        return moves.get(maxPos);
     }
 
     public Move SingleThreadDecision(Board b) {
-        // get maximum move
+        // Same idea as the concurrently implemented decision().
+		// This function considers each move and resultant board state serially.
 
         ArrayList<Move> moves = b.getMoves(color);
         ArrayList<Move> state = new ArrayList<Move>();
@@ -140,27 +163,28 @@ public class MinMax {
             state.remove(state.lastIndexOf(moves.get(i)));
         }
 
-        // max
-        int maxi = -1;
+        int maxPos = -1;
         float max = Float.NEGATIVE_INFINITY;
         for(int i = 0; i < moves.size(); i++) {
             if(costs[i] >= max) {
-                if(Math.abs(costs[i]-max) < 0.1) // add a little random element
+                if(Math.abs(costs[i]-max) < 0.1)
                     if(rand.nextBoolean())
                         continue;
 
                 max = costs[i];
-                maxi = i;
+                maxPos = i;
             }
         }
 
-        if(maxi == -1)
+        if(maxPos == -1)
             return null;
         else
-            return moves.get(maxi);
+            return moves.get(maxPos);
     }
 
-    private float eval1(Board b, ArrayList<Move> moves, boolean currentColor) {
+	// This function attempts to evaluate standing in the game based on values assigned to pieces and their positioning.
+	// (From the perspective of parameter currentColor)
+    private float powerIndex(Board b, ArrayList<Move> moves, boolean currentColor) {
         Tile[][] tiles = b.getTilesAfter(moves);
 
         if(b.getMoves(currentColor).size() == 0) {
@@ -197,8 +221,6 @@ public class MinMax {
 				}
         	}
 		}
-
-		// Hacky way to pass by reference? Perhaps.
 	}
 
 } // MinMax
